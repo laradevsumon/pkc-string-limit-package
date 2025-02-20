aab42dd5336fb9450d892deb0efe422d513fdf40
Roktim Ariyan <sumonyahoo24@gmail.com>
2025-02-21 04:11:42 +0600

    Error fixing

--- src/helpers.php
+++ src/helpers.php
@@ -3,17 +3,12 @@
 use Illuminate\Support\Str;
 
 if (!function_exists('word_limit')) {
-    function word_limit($string, $limit = null, $end = null, $separator = null)
+    function word_limit($string, $limit = null, $end = null)
     {
         $limit = $limit ?? config('wordlimit.default_limit', 100);
         $end = $end ?? config('wordlimit.default_end', '...');
-        $separator = $separator ?? config('wordlimit.default_separator', ' ');
         
-        $words = str_word_count($string, 2, $separator);
-        if (count($words) <= $limit) {
-            return $string;
-        }
-        
-        return implode($separator, array_slice($words, 0, $limit)) . $end;
+        return Str::words($string, $limit, $end);
     }
 }
+
